914eabf655b9bb1f657d9840495420d79eb2f16b
olga210500 <olakovalisko@gmail.com>
2021-12-20 15:08:10 +0200

    Added routes to app-routing.module and deleted them from app.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddTaskComponent } from './pages/add-task/add-task.component';
+import { EditTaskComponent } from './pages/edit-task/edit-task.component';
+import { ToDoListComponent } from './pages/to-do-list/to-do-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ToDoListComponent,
+  },
+  {
+    path: 'addTask',
+    component: AddTaskComponent,
+  },
+  {
+    path:'editTask/:id',
+    component:EditTaskComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
